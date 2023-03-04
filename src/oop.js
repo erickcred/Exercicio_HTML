798e04eb75a600128e095730c5f544f92a28354f
Erick Rick <erickcredjes@gmail.com>
2023-03-04 07:42:29 -0300

    OOP

--- src/oop.js
+++ src/oop.js
@@ -6,6 +6,8 @@
 
 
 class Pokemon {
+    #hp = 100;
+
     constructor(nome, tipo) {
         this.nome = nome;
         this.tipo = tipo;
@@ -14,14 +16,43 @@ class Pokemon {
     atacar(nomeDoAtaque) {
         console.log(`${this.nome} ${nomeDoAtaque}`);
     }
+
+    getHp() {
+        return this.#hp;
+    }
+
+    recebeuAtaque() {
+        this.#hp -= 10;
+    }
 }
 
-const pikachu = new Pokemon("Pikachu", "elétrico");
+class Pikachu extends Pokemon {
+    constructor() {
+        super("Pikachu", "elétrico");
+    }
+
+    atacar() {
+        console.log(`${this.nome} atacou com Choque do Trovão`);
+    }
+}
+
+class Charizard extends Pokemon {
+
+}
+
+const pikachu = new Pokemon("Picachu", "elétrico");
+const pikachuDoAsh = new Pikachu();
+pikachuDoAsh.recebeuAtaque();
+console.log(`HP: ${pikachuDoAsh.getHp()}`);
+pikachuDoAsh.atacar();
+
 pikachu.atacar("Choque do trovão");
 
-const charizard = new Pokemon();
+const charizard = new Charizard();
 charizard.nome = "Charizard";
 charizard.tipo = "fogo";
 
 console.log(pikachu);
 console.log(charizard);
+console.log(pikachu instanceof Pokemon);
+console.log(pikachuDoAsh instanceof Pikachu);
