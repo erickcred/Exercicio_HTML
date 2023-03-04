9e9e3988443bee12ec1587573ed6a9c1d5d4840b
Erick Rick <erickcredjes@gmail.com>
2023-03-04 11:17:34 -0300

    Casting

---
+++ src/casting.js
@@ -0,0 +1,10 @@
+"use strict";
+var casting;
+(function (casting) {
+    let idade = 25;
+    idade.toFixed(2);
+    idade.length;
+    idade.forEach(x => {
+        console.log(x);
+    });
+})(casting || (casting = {}));
--- src/funcoes.js
+++ src/funcoes.js
@@ -4,9 +4,18 @@ function calculaArea(base, altura) {
 }
 const calculaArea2 = (base, altura) => base * altura;
 function somar(...numeros) {
-    return numeros.reduce((total, itemAtual) => {
-        return total += itemAtual;
-    }, 0);
-    // console.log(numeros);
+    // return numeros.reduce((total, itemAtual) => {
+    //     return total += itemAtual;
+    // }, 0);
+    console.log(numeros);
 }
 console.log(somar(1, 2, 10));
+function teste() {
+    if (10 > 5) {
+        return "dez maior que 5";
+    }
+    else {
+        return 5;
+    }
+}
+const resutadoDeTeste = teste(); // pode ser string ou number
